3d730023c501750ded58f485f9c428c886cddf63
Bartosz Nowak <bartekn337@gmail.com>
2022-11-14 00:36:08 +0100

    dev

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,83 @@
 use image::io::Reader as ImageReader;
 // use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
+use image::{ImageBuffer, Rgb, DynamicImage, GenericImage};
+use turbojpeg::OwnedBuf;
 use std::fs::{self};
-use std::io::{Error, ErrorKind, Result};
-use std::path::{Path, PathBuf};
+use std::io::{Error, ErrorKind};
+use std::path::Path;
 
-const TARGET_DIR: &str = "./compressed";
+type CompFn = dyn Fn(&ImageBuffer<Rgb<u8>, Vec<u8>>) -> Result<OwnedBuf, Error>;
 
-type CompFn = dyn Fn(&Path) -> Result<Vec<u8>>;
-
-fn compress(path: &Path, qty: i32) -> Result<Vec<u8>> {
-    let img = ImageReader::open(path)?
-        .decode()
-        .map_err(|err| -> Error { Error::new(ErrorKind::InvalidData, err) })?
-        .into_rgb8();
-    let jpeg_data = turbojpeg::compress_image(&img, qty, turbojpeg::Subsamp::Sub2x2)
-        .map_err(|err| -> Error { Error::new(ErrorKind::InvalidData, err) })?;
-    Ok(jpeg_data.to_vec())
+fn compress(data: &ImageBuffer<Rgb<u8>, Vec<u8>>, qty: i32) -> Result<OwnedBuf, Error> {
+    Ok(
+        turbojpeg::compress_image(data, qty, turbojpeg::Subsamp::Sub2x2)
+            .map_err(|err| -> Error { Error::new(ErrorKind::InvalidData, err) })?
+    )
 }
 
-fn comp_fn(qty: i32) -> Box<CompFn> {
-    Box::new(move |path: &Path| -> Result<Vec<u8>> { compress(path, qty) })
+fn comp_fn_factory(qty: i32) -> Box<CompFn> {
+    Box::new(
+        move |data: &ImageBuffer<Rgb<u8>, Vec<u8>>| -> Result<OwnedBuf, Error> {
+            compress(data, qty)
+        },
+    )
 }
 
-fn target_path(target_rel_path: &str) -> PathBuf {
-    Path::new(format!("{}/{}", TARGET_DIR, target_rel_path).as_str()).to_owned()
+struct Compressor {
+    comp_fn: Box<CompFn>,
 }
 
-fn target_rel_path_conv(src_rel_path: &Path) -> &Path {
-    let mut rel = src_rel_path.iter();
-    rel.next();
-    rel.next();
-    rel.as_path()
-}
-
-fn comp_image(src_rel_path: &Path, comp_fn: &CompFn) -> Result<()> {
-    let comp = comp_fn(src_rel_path)
-        .map_err(|err| -> Error { Error::new(ErrorKind::InvalidData, err) })?;
-    let target = target_path(
-        target_rel_path_conv(src_rel_path)
-            .to_str()
-            .ok_or_else(|| Error::from(ErrorKind::InvalidData))?,
-    );
-    fs::write(target.as_path(), comp)?;
-    Ok(())
-}
-
-fn comp_dir(src_rel_path: &Path, comp_fn: &CompFn) {
-    if !src_rel_path.is_dir() {
-        return;
+impl Compressor {
+    fn new(comp_fn: Box<CompFn>) -> Self {
+        Compressor { comp_fn }
     }
-    fs::read_dir(src_rel_path).unwrap().for_each(|f| {
-        let path = f.unwrap().path();
-        println!("{}", path.as_path().to_str().unwrap());
-        if path.is_file() {
-            comp_image(path.as_path(), comp_fn).unwrap_or_else(|f| {
-                println!("{}", f);
-            });
-        } else if path.is_dir() {
-            fs::create_dir(target_path(
-                target_rel_path_conv(path.as_path()).to_str().unwrap(),
-            ))
-            .unwrap_or_else(|f| {
-                println!("{}", f);
-            });
-            comp_dir(path.as_path(), comp_fn);
-        }
-    })
-}
 
-fn rm_file(target_rel_path: &Path) {
-    fs::remove_file(target_rel_path).unwrap();
-}
-
-fn rm_dir(target_rel_path: &Path) {
-    if !target_rel_path.is_dir() {
-        return;
+    fn comp_image(&self, src_abs_path: &Path, dst_abs_path: &Path) -> Result<(), Error> {
+        let img = ImageReader::open(src_abs_path)?
+            .decode()
+            .map_err(|err| -> Error { Error::new(ErrorKind::InvalidData, err) })?.into_rgb8();
+        let width = img.width();
+        let height = img.height();
+        let vec = img.to_vec();
+        let comp = (self.comp_fn)(&img)
+            .map_err(|err| -> Error { Error::new(ErrorKind::InvalidData, err) })?;
+        println!("{} {}", vec.len(), comp.len());
+        println!("{}", (width as f64)/(height as f64));
+        std::fs::write(dst_abs_path, comp)?;
+        Ok(())
     }
-    fs::read_dir(target_rel_path).unwrap().for_each(|f| {
-        let path = f.unwrap().path();
-        if path.is_file() {
-            rm_file(target_path(target_rel_path_conv(path.as_path()).to_str().unwrap()).as_path());
-        } else if path.is_dir() {
-            fs::remove_dir_all(
-                target_path(target_rel_path_conv(path.as_path()).to_str().unwrap()).as_path(),
-            )
-            .unwrap_or_else(|f| {
-                println!("{}", f);
-            });
+
+    fn comp_dir(&self, src_abs_path: &Path, dst_abs_path: &Path) -> Result<(), Error> {
+        if !src_abs_path.is_dir() {
+            return Err(Error::from(ErrorKind::InvalidData));
         }
-    })
+        fs::read_dir(src_abs_path)?.try_for_each(|f| -> Result<(), Error> {
+            let path = f?.file_name();
+            let src_abs_path_joined = src_abs_path.join(&path);
+            let dst_abs_path_joined = dst_abs_path.join(&path);
+            if src_abs_path_joined.is_file() {
+                self.comp_image(&src_abs_path_joined, &dst_abs_path_joined)
+                    .unwrap_or_else(|f| {
+                        println!("{}", f);
+                    });
+            } else if src_abs_path_joined.is_dir() {
+                fs::create_dir(&dst_abs_path_joined).unwrap_or_else(|f| {
+                    println!("{}", f);
+                });
+                self.comp_dir(&src_abs_path_joined, &dst_abs_path_joined)
+                    .unwrap_or_else(|f| {
+                        println!("{}", f);
+                    });
+            }
+            Ok(())
+        })?;
+        Ok(())
+    }
 }
 
 fn main() {
-    comp_dir(Path::new("./images"), &comp_fn(40));
+    // Compressor::new(comp_fn_factory(1)).comp_image(Path::new("./images/20210813_112050.jpg"), Path::new("./compressed/20210813_112050.jpg"));
+    Compressor::new(comp_fn_factory(1))
+        .comp_dir(Path::new("./images"), Path::new("./compressed"))
+        .unwrap();
 }
