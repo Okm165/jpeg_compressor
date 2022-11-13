c09f3e99e22360b195b638c48ee055f5b23001df
Bartosz Nowak <bartekn337@gmail.com>
2022-11-13 22:29:53 +0100

    dev

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
-use std::fs::{self, read};
-use std::io::Result;
+use image::io::Reader as ImageReader;
+// use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
+use std::fs::{self};
+use std::io::{Error, ErrorKind, Result};
 use std::path::{Path, PathBuf};
-use std::sync::mpsc::channel;
-use std::time::Duration;
 
 const TARGET_DIR: &str = "./compressed";
 
-fn comp_indentity(path: &Path) -> Result<Vec<u8>> {
-    fs::read(path)
+type CompFn = dyn Fn(&Path) -> Result<Vec<u8>>;
+
+fn compress(path: &Path, qty: i32) -> Result<Vec<u8>> {
+    let img = ImageReader::open(path)?
+        .decode()
+        .map_err(|err| -> Error { Error::new(ErrorKind::InvalidData, err) })?
+        .into_rgb8();
+    let jpeg_data = turbojpeg::compress_image(&img, qty, turbojpeg::Subsamp::Sub2x2)
+        .map_err(|err| -> Error { Error::new(ErrorKind::InvalidData, err) })?;
+    Ok(jpeg_data.to_vec())
+}
+
+fn comp_fn(qty: i32) -> Box<CompFn> {
+    Box::new(move |path: &Path| -> Result<Vec<u8>> { compress(path, qty) })
 }
 
 fn target_path(target_rel_path: &str) -> PathBuf {
@@ -22,26 +33,36 @@ fn target_rel_path_conv(src_rel_path: &Path) -> &Path {
     rel.as_path()
 }
 
-fn comp_image(src_rel_path: &Path, comp_fn: fn(&Path) -> Result<Vec<u8>>) {
-    let comp = comp_fn(src_rel_path).unwrap();
-    let target = target_path(target_rel_path_conv(src_rel_path).to_str().unwrap());
-    fs::write(target.as_path(), comp).unwrap();
+fn comp_image(src_rel_path: &Path, comp_fn: &CompFn) -> Result<()> {
+    let comp = comp_fn(src_rel_path)
+        .map_err(|err| -> Error { Error::new(ErrorKind::InvalidData, err) })?;
+    let target = target_path(
+        target_rel_path_conv(src_rel_path)
+            .to_str()
+            .ok_or_else(|| Error::from(ErrorKind::InvalidData))?,
+    );
+    fs::write(target.as_path(), comp)?;
+    Ok(())
 }
 
-fn comp_dir(src_rel_path: &Path, comp_fn: fn(&Path) -> Result<Vec<u8>>) {
+fn comp_dir(src_rel_path: &Path, comp_fn: &CompFn) {
     if !src_rel_path.is_dir() {
         return;
     }
-    fs::read_dir(src_rel_path).unwrap().for_each(|f| -> () {
+    fs::read_dir(src_rel_path).unwrap().for_each(|f| {
         let path = f.unwrap().path();
         println!("{}", path.as_path().to_str().unwrap());
         if path.is_file() {
-            comp_image(path.as_path(), comp_fn);
+            comp_image(path.as_path(), comp_fn).unwrap_or_else(|f| {
+                println!("{}", f);
+            });
         } else if path.is_dir() {
             fs::create_dir(target_path(
                 target_rel_path_conv(path.as_path()).to_str().unwrap(),
             ))
-            .unwrap();
+            .unwrap_or_else(|f| {
+                println!("{}", f);
+            });
             comp_dir(path.as_path(), comp_fn);
         }
     })
@@ -55,21 +76,21 @@ fn rm_dir(target_rel_path: &Path) {
     if !target_rel_path.is_dir() {
         return;
     }
-    fs::read_dir(target_rel_path).unwrap().for_each(|f| -> () {
+    fs::read_dir(target_rel_path).unwrap().for_each(|f| {
         let path = f.unwrap().path();
         if path.is_file() {
-            rm_file(
-                target_path(target_rel_path_conv(path.as_path()).to_str().unwrap()).as_path(),
-            );
+            rm_file(target_path(target_rel_path_conv(path.as_path()).to_str().unwrap()).as_path());
         } else if path.is_dir() {
             fs::remove_dir_all(
                 target_path(target_rel_path_conv(path.as_path()).to_str().unwrap()).as_path(),
             )
-            .unwrap();
+            .unwrap_or_else(|f| {
+                println!("{}", f);
+            });
         }
     })
 }
 
 fn main() {
-    comp_dir(Path::new("./temp"), comp_indentity);
+    comp_dir(Path::new("./images"), &comp_fn(40));
 }
