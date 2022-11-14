96541bcc4c089570e4be4585867454d7bc83bc3e
Bartosz Nowak <bartekn337@gmail.com>
2022-11-14 18:49:17 +0100

    def

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use image::io::Reader as ImageReader;
-// use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
 use image::{ImageBuffer, Rgb};
+use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
+use pathdiff::diff_paths;
+use std::env;
 use std::fs::{self};
 use std::io::{Error, ErrorKind};
-use std::path::Path;
-use turbojpeg::{OwnedBuf};
+use std::path::{Path, PathBuf};
+use std::time::Duration;
+use tokio::sync::mpsc;
+use tokio::task::JoinHandle;
+use turbojpeg::OwnedBuf;
 
-type CompFn = dyn Fn(&ImageBuffer<Rgb<u8>, Vec<u8>>) -> Result<OwnedBuf, Error>;
+type CompFn = dyn Fn(&ImageBuffer<Rgb<u8>, Vec<u8>>) -> Result<OwnedBuf, Error> + Send;
 
 fn compress(data: &ImageBuffer<Rgb<u8>, Vec<u8>>, qty: i32) -> Result<OwnedBuf, Error> {
-    Ok(
-        turbojpeg::compress_image(data, qty, turbojpeg::Subsamp::Sub2x2)
-            .map_err(|err| -> Error { Error::new(ErrorKind::InvalidData, err) })?,
-    )
+    turbojpeg::compress_image(data, qty, turbojpeg::Subsamp::Sub2x2)
+        .map_err(|err| -> Error { Error::new(ErrorKind::InvalidData, err) })
 }
 
 fn comp_fn_factory(qty: i32) -> Box<CompFn> {
@@ -33,49 +36,147 @@ impl Compressor {
     }
 
     fn comp_image(&self, src_abs_path: &Path, dst_abs_path: &Path) -> Result<(), Error> {
-        let img = ImageReader::open(src_abs_path)?
-            .decode()
-            .map_err(|err| -> Error { Error::new(ErrorKind::InvalidData, err) })?
-            .into_rgb8();
+        let img = self.read(src_abs_path)?;
         let comp = (self.comp_fn)(&img)
             .map_err(|err| -> Error { Error::new(ErrorKind::InvalidData, err) })?;
-
-        fs::write(dst_abs_path, comp)?;
-        println!("{}", src_abs_path.to_str().unwrap());
+        
+        self.save(dst_abs_path, comp)?;
         Ok(())
     }
 
-    fn comp_dir(& mut self, src_abs_path: &Path, dst_abs_path: &Path) -> Result<(), Error> {
-        if !src_abs_path.is_dir() {
-            return Err(Error::from(ErrorKind::InvalidData));
+    fn read(&self, src_abs_path: &Path) -> Result<ImageBuffer<Rgb<u8>, Vec<u8>>, Error> {
+        Ok(ImageReader::open(src_abs_path)?
+            .decode()
+            .map_err(|err| -> Error { Error::new(ErrorKind::InvalidData, err) })?
+            .into_rgb8())
+    }
+
+    fn save(&self, dst_abs_path: &Path, buf: OwnedBuf) -> Result<(), Error> {
+        let prefix = dst_abs_path
+            .parent()
+            .ok_or_else(|| Error::from(ErrorKind::InvalidData))?;
+        std::fs::create_dir_all(prefix)?;
+        std::fs::write(dst_abs_path, buf)?;
+        Ok(())
+    }
+}
+#[derive(Clone, Debug)]
+enum CompressorAction {
+    Create(PathBuf),
+    Remove(PathBuf),
+    Rename(PathBuf, PathBuf),
+}
+
+struct CompressorScheduler {
+    comp_tasks: Vec<(JoinHandle<()>, mpsc::UnboundedSender<CompressorAction>)>,
+}
+
+impl CompressorScheduler {
+    fn new() -> Self {
+        CompressorScheduler {
+            comp_tasks: Vec::new(),
         }
-        fs::read_dir(src_abs_path)?.try_for_each(|f| -> Result<(), Error> {
-            let path = f?.file_name();
-            let src_abs_path_joined = src_abs_path.join(&path);
-            let dst_abs_path_joined = dst_abs_path.join(&path);
-            if src_abs_path_joined.is_file() {
-                self.comp_image(&src_abs_path_joined, &dst_abs_path_joined)
-                    .unwrap_or_else(|f| {
-                        println!("{}", f);
-                    });
-            } else if src_abs_path_joined.is_dir() {
-                fs::create_dir(&dst_abs_path_joined).unwrap_or_else(|f| {
-                    println!("{}", f);
-                });
-                self.comp_dir(&src_abs_path_joined, &dst_abs_path_joined)
-                    .unwrap_or_else(|f| {
-                        println!("{}", f);
-                    });
+    }
+
+    fn add_compressor(&mut self, comp: Compressor, dest_folder: PathBuf) {
+        let (tx, mut rx) = mpsc::unbounded_channel::<CompressorAction>();
+        let task = tokio::spawn(async move {
+            while let Some(event) = rx.recv().await {
+                println!("{} -- {:?}", dest_folder.to_str().unwrap(), event);
+                match event {
+                    CompressorAction::Create(path) => {
+                        comp.comp_image(path.as_path(), dest_folder.join(path.as_path()).as_ref())
+                            .unwrap_or_else(|err| println!("{}", err));
+                    }
+                    CompressorAction::Remove(path) => {
+                        if dest_folder.join(path.as_path()).as_path().is_dir() {
+                            fs::remove_dir_all::<&Path>(dest_folder.join(path.as_path()).as_ref())
+                                .unwrap_or_else(|err| println!("{}", err));
+                        } else {
+                            fs::remove_file::<&Path>(dest_folder.join(path.as_path()).as_ref())
+                                .unwrap_or_else(|err| println!("{}", err));
+                        }
+                    }
+                    CompressorAction::Rename(from, to) => {
+                        fs::rename::<&Path, &Path>(
+                            dest_folder.join(from.as_path()).as_ref(),
+                            dest_folder.join(to.as_path()).as_ref(),
+                        )
+                        .unwrap_or_else(|err| println!("{}", err));
+                    }
+                }
             }
-            Ok(())
-        })?;
+        });
+        self.comp_tasks.push((task, tx));
+    }
+    fn remove_compressor(&mut self, index: usize) -> Result<(), Error> {
+        self.comp_tasks
+            .get(index)
+            .ok_or_else(|| Error::from(ErrorKind::InvalidData))?
+            .0
+            .abort();
+        self.comp_tasks.remove(index);
+        Ok(())
+    }
+
+    fn clear(&mut self) {
+        for (handle, _) in self.comp_tasks.iter() {
+            handle.abort();
+        }
+        self.comp_tasks.clear();
+    }
+
+    fn notify_all(&self, action: CompressorAction) -> Result<(), Error> {
+        for comp in self.comp_tasks.iter() {
+            comp.1
+                .send(action.clone())
+                .map_err(|err| -> Error { Error::new(ErrorKind::BrokenPipe, err) })?;
+        }
         Ok(())
     }
 }
 
-fn main() {
-    Compressor::new(comp_fn_factory(10))
-        .comp_dir(Path::new("./images"), Path::new("./compressed"))
-        .unwrap();
-    
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let mut scheduler = CompressorScheduler::new();
+    scheduler.add_compressor(
+        Compressor::new(comp_fn_factory(50)),
+        Path::new("compressed60").to_path_buf(),
+    );
+    scheduler.add_compressor(
+        Compressor::new(comp_fn_factory(10)),
+        Path::new("compressed20").to_path_buf(),
+    );
+
+    let dir = Path::new("./temp");
+    let work_dir = env::current_dir()?;
+
+    let (tx, rx) = std::sync::mpsc::channel();
+    let mut watcher = watcher(tx, Duration::from_secs(2)).unwrap();
+    watcher.watch(dir, RecursiveMode::Recursive).unwrap();
+
+    while let Ok(event) = rx.recv() {
+        let action = match event {
+            DebouncedEvent::Create(path) => Ok(CompressorAction::Create(
+                diff_paths(path, work_dir.clone()).unwrap(),
+            )),
+            DebouncedEvent::Remove(path) => Ok(CompressorAction::Remove(
+                diff_paths(path, work_dir.clone()).unwrap(),
+            )),
+            DebouncedEvent::Rename(from, to) => Ok(CompressorAction::Rename(
+                diff_paths(from, work_dir.clone()).unwrap(),
+                diff_paths(to, work_dir.clone()).unwrap(),
+            )),
+            _ => Err(Error::from(ErrorKind::Unsupported)),
+        };
+
+        if action.is_err() {
+            continue;
+        }
+        scheduler.notify_all(action.unwrap()).unwrap();
+    }
+
+    scheduler.clear();
+
+    Ok(())
 }
